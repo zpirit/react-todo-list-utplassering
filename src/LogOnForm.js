22dbae1b964a4f22c0b4c142f08b0481ed7ded33
Øyvind Eikeland <oyvind@zpirit.no>
2021-11-11 12:51:12 +0100

    Added LogOnFOrm as example

---
+++ src/LogOnForm.js
@@ -0,0 +1,47 @@
+import React from 'React';
+
+class LogOnForm extends React.Component {
+	construtor(props) {
+		super(props);
+		this.state = {
+			username: null,
+			password: null
+		}
+	}
+
+	setUsername(user) {
+		this.setState({ username: user });
+	}
+
+	setPassword(pass) {
+		this.setState({ password: pass });
+	}
+
+	handleSubmit(){
+		console.log('Submitting Form');
+		console.log('Username', this.state.username);
+		console.log('Password', this.state.password);
+	}
+
+	render() {
+		return (
+			<>
+				<form onSubmit={handleSubmit}>
+					<label>
+						Username:
+						<input type="text" value={username} onChange={(e) => {setUsername(e.target.value)}}/>
+					</label>
+
+					<label>
+						Password:
+						<input type="text" value={password} onChange={(e) => {setPassword(e.target.value)}}/>
+					</label>
+
+					<button type="submit">
+						Submit
+					</button>
+				</form>
+			</>
+		)
+	}
+}
